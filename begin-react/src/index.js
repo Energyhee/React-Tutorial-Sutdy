366c2ddf9c3481f08710c9f414f23948d2d68791
hee <nergyhee@naver.com>
2023-08-31 17:54:16 +0900

    Tic-Tac-Toe setting

--- begin-react/src/index.js
+++ begin-react/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Ttt from './Ttt';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Ttt />
   </React.StrictMode>
 );
